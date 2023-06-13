cc234a45aff0a344439fc93c0668d73b9050832f
brand421 <r3v9191@gmail.com>
2023-06-13 18:23:07 -0400

    fixed babel error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
     return (
-        <nav>
-            <div>
-               <h1 id="nameHeader">Brandan Isaacs</h1> 
-            </div>
-            <div>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Projects</a></li>
-                    <li><a href="#">About Me</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
+        <nav className="navbar">
+            <div className="container">
+                <div className="logo">
+                    <h1 id="nameHeader">Brandan Isaacs</h1>
+                </div>
+                <div className="nav-elements">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Projects</a></li>
+                        <li><a href="#">About Me</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
