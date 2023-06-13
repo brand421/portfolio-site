1fe1008b4a7d1d74a8d8c5228315cd1fb076e650
brand421 <r3v9191@gmail.com>
2023-06-13 18:16:07 -0400

    create basic framework for Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 function Navbar() {
-
+    return (
+        <nav>
+            <div>
+               <h1 id="nameHeader">Brandan Isaacs</h1> 
+            </div>
+            <div>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">About Me</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
