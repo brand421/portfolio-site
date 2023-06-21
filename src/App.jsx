a150c4535b8e2daf1f97e6d11857ffb6433cff76
brand421 <57531511+brand421@users.noreply.github.com>
2023-06-21 15:44:26 -0400

    add portrait to sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import SideMenu from "./components/Header/Header";
 import Landing from "./components/Landing/Landing";
-import { SideMenuData } from "./components/Data";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <SideMenu>
-        <Routes>
-          <Route path="/" element={<DynamicMenuItems page="homepage" />} />
-          {}
-        </Routes>
-        <Landing />
-      </SideMenu>
+      <SideMenu />
+      <Landing />
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import portrait from "../../images/Brandon.jpg";
 import "./header.css";
 
 function SideMenu() {
+  const [sidebar, setSidebar] = useState(false);
+
+  function handleSidebarOpen() {
+    setSidebar(true);
+  }
+
+  function handleSidebarClose() {
+    setSidebar(false);
+  }
   return (
     <header className="side__menu">
       <nav className="nav container">
+        <div className="portait">
+          <img src={portrait} alt="Brandan portrait" />
+        </div>
         <div className="nav__menu">
           <ul className="nav__list grid">
             <li className="nav__item">
               <a href="#Home" className="nav__link active__link">
-                <i className="bx bx-home-alt nav__icon"></i>Home
+                <i className="bx bx-home-alt nav__icon"></i>
+                <p>Home</p>
               </a>
             </li>
             <li className="nav__item">
