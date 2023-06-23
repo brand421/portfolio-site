1d934cc2db2464dd5e2e3cb52f4728f8621184f8
brand421 <57531511+brand421@users.noreply.github.com>
2023-06-23 12:46:56 -0400

    create sidebar and sidebar links

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Sidebar />
       <Home />
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,96 +1,113 @@
 import {
-  HomeOutlined,
-  DataObjectOutlined,
-  Menu,
-  PersonOutlineOutlined,
-  EmailOutlined,
-} from "@mui/icons-material";
-import {
-  Box,
-  Toolbar,
-  Avatar,
+  //   Box,
+  //   Toolbar,
+  //   Avatar,
   IconButton,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  CssBaseline,
-  Drawer,
-  ListItemButton,
-  AppBar,
+  //   List,
+  //   ListItem,
+  //   ListItemIcon,
+  //   ListItemText,
+  //   CssBaseline,
+  //   Drawer,
+  //   ListItemButton,
+  //   AppBar,
 } from "@mui/material";
 import portrait from "../../images/Brandon.jpg";
+import sidebarItems from "./sidebarItems";
+import { Link } from "react-router-dom";
 import "./sidebar.css";
 import React, { useState } from "react";
 
 export default function Sidebar() {
-  const listItems = [
-    {
-      listIcon: <HomeOutlined />,
-      listText: "Home",
-    },
-    {
-      listIcon: <DataObjectOutlined />,
-      listText: "Projects",
-    },
-    {
-      listIcon: <PersonOutlineOutlined />,
-      listText: "About",
-    },
-    {
-      listIcon: <EmailOutlined />,
-      listText: "Contact",
-    },
-  ];
+  const [active, setActive] = useState(0);
 
-  const [open, setOpen] = useState(false);
-
-  function toggleSlider() {
-    setOpen(!open);
-  }
-
-  const sideList = () => {
-    <Box
-      className="menu__slider"
-      sx={{ width: 250 }}
-      onClick={() => setOpen(false)}
-    >
-      <Avatar className="avatar" src={portrait} alt="portrait of Brandan" />
-      <List>
-        {listItems.map((listItem, index) => (
-          <ListItem className="listItem" key={listItem}>
-            <ListItemButton>
-              <ListItemIcon>{listItem.listIcon}</ListItemIcon>
-              <ListItemText primary={listItem.listText} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>;
-  };
+  function setCurrentPage(page) {}
 
   return (
-    <>
-      <CssBaseline />
-
-      <Box component="nav" className="header">
-        <AppBar
-          position="static"
-          sx={{ backgroundColor: "transparent", boxShadow: "none" }}
-        >
-          <Toolbar>
-            <IconButton
-              onClick={toggleSlider}
-              sx={{ backgroundColor: "#e0e0e0" }}
-            >
-              <Menu />
-            </IconButton>
-            <Drawer open={open} anchor="left" onClose={toggleSlider}>
-              {sideList}
-            </Drawer>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </>
+    <nav id="sidebar__container">
+      <div className="photo__div">
+        <img id="photo" src={portrait} alt="Brandan portrait" />
+      </div>
+      <div className="menu__items">
+        {sidebarItems.map((item, index) => (
+          <Link to={item.itemPath} className="link" key={index}>
+            <IconButton>{item.itemIcon}</IconButton>
+            <h3 className="menu__item">{item.itemName}</h3>
+          </Link>
+        ))}
+      </div>
+    </nav>
   );
 }
+
+// export default function Sidebar() {
+//   const listItems = [
+//     {
+//       listIcon: <HomeOutlined />,
+//       listText: "Home",
+//     },
+//     {
+//       listIcon: <DataObjectOutlined />,
+//       listText: "Projects",
+//     },
+//     {
+//       listIcon: <PersonOutlineOutlined />,
+//       listText: "About",
+//     },
+//     {
+//       listIcon: <EmailOutlined />,
+//       listText: "Contact",
+//     },
+//   ];
+
+//   const [open, setOpen] = useState(false);
+
+//   function toggleSlider() {
+//     setOpen(!open);
+//   }
+
+//   const sideList = () => {
+//     <Box
+//       className="menu__slider"
+//       sx={{ width: 250 }}
+//       onClick={() => setOpen(false)}
+//     >
+//       <Avatar className="avatar" src={portrait} alt="portrait of Brandan" />
+//       <List>
+//         {listItems.map((listItem, index) => (
+//           <ListItem className="listItem" key={listItem}>
+//             <ListItemButton>
+//               <ListItemIcon>{listItem.listIcon}</ListItemIcon>
+//               <ListItemText primary={listItem.listText} />
+//             </ListItemButton>
+//           </ListItem>
+//         ))}
+//       </List>
+//     </Box>;
+//   };
+
+//   return (
+//     <>
+//       <CssBaseline />
+
+//       <Box component="nav" className="header">
+//         <AppBar
+//           position="static"
+//           sx={{ backgroundColor: "transparent", boxShadow: "none" }}
+//         >
+//           <Toolbar>
+//             <IconButton
+//               onClick={toggleSlider}
+//               sx={{ backgroundColor: "#e0e0e0" }}
+//             >
+//               <Menu />
+//             </IconButton>
+//             <Drawer open={open} anchor="left" onClose={toggleSlider}>
+//               {sideList}
+//             </Drawer>
+//           </Toolbar>
+//         </AppBar>
+//       </Box>
+//     </>
+//   );
+// }
