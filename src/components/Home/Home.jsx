e0a19ab64adb6e72507ea19c272d5d86e2a8ad00
brand421 <57531511+brand421@users.noreply.github.com>
2023-06-26 10:46:53 -0400

    update home.jsx

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,21 @@
+import "./home.css";
+import React from "react";
+import mountainPhoto from "../../images/greeting_square.JPG";
+
+function Home() {
+  return (
+    <div className="home__container">
+      <div className="home__item"></div>
+      <div className="home__item home__text">
+        <h1>Hi, I'm Brandan</h1>
+        <h3>A Full Stack Developer</h3>
+      </div>
+      <div className="home__item">
+        <img src={mountainPhoto} alt="" />
+      </div>
+      <div className="home__item"></div>
+    </div>
+  );
+}
+
+export default Home;
