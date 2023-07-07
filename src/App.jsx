5844c3d7d64a9d7145b44908fb078c89f49eddba
brand421 <57531511+brand421@users.noreply.github.com>
2023-07-07 16:32:33 -0400

    rework sidebar functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar/Sidebar";
-import AnimatedRoutes from "./components/AnimatedRoutes";
+
 import Footer from "./components/Footer/Footer";
+import Contact from "./components/Contact/Contact";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
     <BrowserRouter>
-      <Sidebar />
-      <AnimatedRoutes />
+      <Sidebar>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Sidebar>
       <Footer />
     </BrowserRouter>
   );
