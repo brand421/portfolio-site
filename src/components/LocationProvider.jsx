37f153c5ea97708dad48d07f8a259c0dad559361
brand421 <57531511+brand421@users.noreply.github.com>
2023-07-14 12:21:55 -0400

    remove unnecessary file

--- src/components/LocationProvider.jsx
+++
@@ -1,8 +0,0 @@
-// import { AnimatePresence } from "framer-motion";
-// import React from "react";
-
-// function LocationProvider({ children }) {
-//   return <AnimatePresence mode="wait">{children}</AnimatePresence>;
-// }
-
-// export default LocationProvider;
